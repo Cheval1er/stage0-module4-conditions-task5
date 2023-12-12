1fe9aaf4106e102fd499e24a258ff60c4cca6f83
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2023-12-12 10:30:57 +0400

    Module #4.1. Task #5. Switch

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,31 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int remainderBy4 = year % 4;
+        int remainderBy100 = year % 100;
+
+        switch (remainderBy4) {
+            case 0:
+                switch (remainderBy100) {
+                    case 0:
+                        switch (year % 400) {
+                            case 0:
+                                System.out.println("leap");
+                                break;
+                            default:
+                                System.out.println("not leap");
+                                break;
+                        }
+                        break;
+                    default:
+                        System.out.println("leap");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
-}
+    }
+
